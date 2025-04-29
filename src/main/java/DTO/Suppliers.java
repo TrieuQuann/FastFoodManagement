fb60c53ee2fe8a3d9681eda30539c8b4db1f86ce
Hoangdangkhoagh <xxxxdangkhoaxxxx@gmail.com>
2025-04-29 15:54:44 +0700

    Update Suppliers.java

--- src/main/java/DTO/Suppliers.java
+++ src/main/java/DTO/Suppliers.java
@@ -9,5 +9,56 @@ package DTO;
  * @author Lenovo
  */
 public class Suppliers {
+   private int supplier_id;
+   private String supplier_name;
+   private String email;
+   private String address;
+   private String phone;
     
+ public Customers(int , String , String , String , String ) {
+        this.supplier_id = supplier_id;
+        this.supplier_name = supplier_name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+public int getsupplier_id() {
+        return supplier_id;
+    }
+
+    public void set(int supplier_id) {
+        this.supplier_id = supplier_id;
+    }
+
+    public String getsupplier_name() {
+        return supplier_name;
+    }
+
+    public void set(String supplier_name) {
+        this.supplier_name = supplier_name;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void set(String email) {
+        this.email = email;
+    }
+
+    public String getaddress() {
+        return address;
+    }
+
+    public void set(String address) {
+        this.address = address;
+    }
+    public String getphone() {
+        return phone;
+    }
+
+    public void set(String phone) {
+        this.phone = phone;
+    }
 }
