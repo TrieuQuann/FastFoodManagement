d7e0457527510320dac13e1aca6fd6f22e3f60df
Hoangdangkhoagh <xxxxdangkhoaxxxx@gmail.com>
2025-04-29 16:24:12 +0700

    Update InvenDetail.java

--- src/main/java/DTO/InvenDetail.java
+++ src/main/java/DTO/InvenDetail.java
@@ -9,5 +9,36 @@ package DTO;
  * @author Lenovo
  */
 public class InvenDetail {
-    
+     private int inven_id;
+     private int invoice_id;
+     private int quantity;
+ public InvenDetail(int inven_id, int invoice_id, int quantity) {
+        this.inven_id = inven_id;
+        this.invoice_id = invoice_id;
+        this.quantity = quantity;
+    }
+
+public int getinven_id() {
+        return inven_id;
+    }
+
+    public void set(int inven_id) {
+        this.inven_id = inven_id;
+    }
+
+    public int getinvoice_id() {
+        return invoice_id;
+    }
+
+    public void set(int invoice_id) {
+        this.invoice_id = invoice_id;
+    }
+
+    public int getquantity() {
+        return quantity;
+    }
+
+    public void set(int quantity) {
+        this.quantity = quantity;
+    }
 }
