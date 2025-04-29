8e55a9c1963527aef142d148a4c0638a5bed3565
Hoangdangkhoagh <xxxxdangkhoaxxxx@gmail.com>
2025-04-29 16:07:01 +0700

    Update InvoiceInventory.java

--- src/main/java/DTO/InvoiceInventory.java
+++ src/main/java/DTO/InvoiceInventory.java
@@ -9,5 +9,47 @@ package DTO;
  * @author Lenovo
  */
 public class InvoiceInventory {
-    
+    private int invoice_id;
+    private int supplier_id;
+    private int quantityadded;
+    private Date date;
+ public InvoiceInventory(int invoice_id, int supplier_id , int quantityadded , Date date) {
+        this.invoice_id = invoice_id;
+        this.supplier_id = supplier_id;
+        this.quantityadded = quantityadded;
+        this.date = date;
+    }
+
+public int getinvoice_id() {
+        return invoice_id;
+    }
+
+    public void set(int invoice_id) {
+        this.invoice_id = invoice_id;
+    }
+
+    public int getsupplier_id() {
+        return supplier_id;
+    }
+
+    public void set(int supplier_id) {
+        this.supplier_id = supplier_id;
+    }
+
+    public int getquantityadded() {
+        return quantityadded;
+    }
+
+    public void set(int quantityadded) {
+        this.quantityadded = quantityadded;
+    }
+
+    public Date getdate() {
+        return date;
+    }
+
+    public void set(Date date) {
+        this.date = date;
+    }
+ 
 }
