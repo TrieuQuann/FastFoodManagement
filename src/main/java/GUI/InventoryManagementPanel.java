5dc2264915f9302a4d8777ca4b8aa72d1fc46cf5
Hoangdangkhoagh <xxxxdangkhoaxxxx@gmail.com>
2025-05-06 14:33:24 +0700

    Update InventoryManagementPanel.java

--- src/main/java/GUI/InventoryManagementPanel.java
+++ src/main/java/GUI/InventoryManagementPanel.java
@@ -1,5 +1,13 @@
 package GUI;
 
+import BUS.InventoryBUS;
+import DTO.Inventory;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.List;
+
 public class InventoryManagementPanel {
     
 }
